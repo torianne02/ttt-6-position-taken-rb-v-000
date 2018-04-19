1ca1f99b1165def99432e6cc5d1119a55545f57c
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-04-19 17:26:02 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,8 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
   if board[index] == " " || "" || nil
-    puts false
+    return false
   elsif board[index] == "X" || "O"
-    puts true
+    return true
   end
 end
